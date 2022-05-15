1fdf7748e11576a0e60c60bdb088148249e8c27f
zahrabahmanpour <z.bmnpr@gmail.com>
2022-05-15 13:32:44 +0430

    add node server

---
+++ README.md
@@ -0,0 +1,11 @@
+**\_\_dirname
+**\_\_filename
+require('./filename)
+module.exports = {obj}
+const os = require('os');
+const fs = require('fs');
+fs.readFile - fs.writeFile - fs.mkdir - fs.existsSync - fs.rmdir
+const http = require('http')
+127.0.0.1 loopback ip address
+const server = http.createServer((req, res) => {})
+server.listen(3000, 'localhost', () => {})
---
+++ server.js
@@ -0,0 +1,36 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.statusCode = 200;
+  let path = './views/';
+  switch (req.url) {
+    case '/':
+      path += 'index.html';
+      break;
+    case '/about':
+      path += 'about.html';
+      break;
+    case '/about-me':
+      res.statusCode = 301;
+      res.setHeader('Location', '/about');
+      break;
+    default:
+      path += '404.html';
+      res.statusCode = 404;
+      break;
+  }
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.write(data);
+    }
+    res.end();
+  });
+});
+
+server.listen(3000, 'localhost', () => {
+  console.log('listening to requests on port 3000');
+});
