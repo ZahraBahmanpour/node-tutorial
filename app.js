134d70a6f6403e3d83ba0962b64e4bc7400d066d
ZahraBahmanpour <z.bmnpr@gmail.com>
2022-10-13 00:24:12 +0330

    remove unwanted comment

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-// add a line
+
 app.listen(3000);
 app.get("/", (req, res) => {
   res.sendFile("./views/index.html", { root: __dirname });
