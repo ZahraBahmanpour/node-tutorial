b921f45a81893625d2f2687dc0282cc8c460f7d2
ZahraBahmanpour <z.bmnpr@gmail.com>
2022-10-12 23:40:46 +0330

    m

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+// add a line
 app.listen(3000);
-app.get('/', (req, res) => {
-  res.sendFile('./views/index.html', { root: __dirname });
+app.get("/", (req, res) => {
+  res.sendFile("./views/index.html", { root: __dirname });
 });
-app.get('/about', (req, res) => {
-  res.sendFile('./views/about.html', { root: __dirname });
+app.get("/about", (req, res) => {
+  res.sendFile("./views/about.html", { root: __dirname });
 });
-app.get('/about-me', (req, res) => {
-  res.redirect('/about');
+app.get("/about-me", (req, res) => {
+  res.redirect("/about");
 });
 app.use((req, res) => {
-  res.status(404).sendFile('./views/404.html', { root: __dirname });
+  res.status(404).sendFile("./views/404.html", { root: __dirname });
 });
